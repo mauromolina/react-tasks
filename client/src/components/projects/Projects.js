8906998efb92d6037ae09793e1f697dd1c9174ed
Mauro <molinamauro2@gmail.com>
2020-08-18 02:43:10 -0300

    added conditionally responsive components

--- client/src/components/projects/Projects.js
+++ client/src/components/projects/Projects.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Sidebar from '../layout/Sidebar';
 import Navbar from '../layout/Navbar';
 import TaskForm from '../tasks/TaskForm';
 import TaskList from '../tasks/TaskList';
  
 const Projects = () => {
+
+    const [size, setSize] = useState({
+        'windowWidth': 0,
+        'windowHeight': 0
+    })
+
+     useEffect( () => {
+         let windowWidth = typeof window !== "undefined" ? window.innerWidth : 0;
+         let windowHeight = typeof window !== "undefined" ? window.innerHeight : 0;
+
+         setSize({ windowWidth, windowHeight });
+     },[])
+
+    
+
     return ( 
         <div className="contenedor-app">
-            <Sidebar/>
+            { size.windowWidth < 768 ? null : <Sidebar/>}
             <div className="seccion-principal">
                 <Navbar/>
                 <main>
@@ -17,6 +32,7 @@ const Projects = () => {
                     </div>
                 </main>
             </div>
+            { size.windowWidth < 768 ? <Sidebar/> : null}
         </div>
      );
 }
