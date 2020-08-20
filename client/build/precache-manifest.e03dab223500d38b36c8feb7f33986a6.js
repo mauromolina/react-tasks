ff9970fe351bff0f3260d978fb0d9cd363be67a9
Mauro <molinamauro2@gmail.com>
2020-08-20 03:00:20 -0300

    add build folder and env

---
+++ client/build/precache-manifest.e03dab223500d38b36c8feb7f33986a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd03069242e75b9c843d37e775ee08cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81357fadab537687ebe5",
+    "url": "/static/css/main.164628de.chunk.css"
+  },
+  {
+    "revision": "388e90007c2448cda2d9",
+    "url": "/static/js/2.422b0185.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.422b0185.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81357fadab537687ebe5",
+    "url": "/static/js/main.bbda2b3e.chunk.js"
+  },
+  {
+    "revision": "a89b4b79558ffc1be4d2",
+    "url": "/static/js/runtime-main.58ddbd37.js"
+  }
+]);
